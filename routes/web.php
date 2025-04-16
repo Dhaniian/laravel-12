bc7bd458d1a927c3feae840d59ad683e4576f8aa
Dhaniian <111202214153@mhs.dinus.ac.id>
2025-04-16 15:28:07 +0700

    Tugas4

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Http\Controllers\ObatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.main');
 });
 Route::get('/dokter', function () {
-    return view('list-dokter');
+    return view('layouts.list_dokter');
 });
-Route::get('/obat', function () {
-    return view('list-obat');
-});
+Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
+    Route::get('/obat/{id}/edit', [ObatController::class, 'edit'])->name('obat.edit');
+    Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
+    Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
+    Route::get('/obat/create', [ObatController::class, 'create'])->name('obat.create');
+    Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.destroy');
